363453fb66e76d7810ef9d37c570bebae1b5b556
IdaliaGpe <idaliaguadalupepucca@hotmail.com>
2020-08-21 08:49:16 -0700

    publish

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,47 @@
 #include<iostream>
 
+void PrintHello(const char* charArr)
+{
+    std::cout << charArr;
+}
+
+int add(int a, int b)
+{
+    return a + b ;
+}
+
+void PrintArrStr(const char* arrString)
+{
+    for(int i = 0; i < 5; i++)
+    {
+        std::cout << arrString +i << std::endl;
+    }
+}
+
 int main()
 {
-    std::cout << "hello";
+    //[0][1][2]
+    //int intArr[3]{4, 100, 42};
+
+    //std::cout << intArr << std::endl;
+
+    //char character{96};
+
+    //char charArr[]{'h', 'e', 'l', 'l', 'o'};
+    //char charArr2[]{"hello"};
+
+    const char*myString{"Hello"};
+
+    //PrintHello(myString);
+
+    /*for(int i = 0; i < 5; i++)
+    {
+        std::cout <<charArr2 +i << std::endl;
+    }*/
+
+    PrintArrStr(myString);
+
+    //std::cout << "add"; << add(1,2) << std::endl;
     
     std::cin.get();
 
